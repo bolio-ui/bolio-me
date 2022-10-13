2b75dcf62b07bb89b599b3681342b5bfb3fe9995
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-10-13 20:01:11 -0300

    add configs to start

---
+++ next-seo.config.js
@@ -0,0 +1,18 @@
+const DefaultSEO = {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://me.bolio-ui.com/',
+    site_name: 'Bolio Me',
+    title: 'Bolio Me - Just enter your username and see what happens',
+    description:
+      'Use Bolio Me to create your developer portfolio with just your username.'
+  },
+  twitter: {
+    handle: '@bolio_ui',
+    site: '@bolio_ui',
+    cardType: 'summary_large_image'
+  }
+}
+
+export default DefaultSEO
