ff82def679b46e71755714a803f504a3f0d45064
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-10-14 08:38:19 -0300

    update page

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Section, Container, Grid, Input } from '@bolio-ui/core'
+import { Section, Container, Grid, Input, Button } from '@bolio-ui/core'
 import Base from 'src/templates/Base'
 import Hero from 'src/components/Hero'
-import * as Icons from '@bolio-ui/icons'
+import { ArrowRight } from '@bolio-ui/icons'
 
 function Home() {
   return (
@@ -13,18 +13,28 @@ function Home() {
           description: 'Just enter your username and see what happens. 🥷🏼'
         }}
       />
-      <Section pb={4}>
+      <Section>
         <Container>
-          <Grid.Container justify="center">
-            <Grid xs={12} md={6}>
+          <Grid.Container gap={2} justify="center" alignItems="center">
+            <Grid xs={12} md={4}>
               <Input
                 width="100%"
-                icon={<Icons.Github />}
                 placeholder="Github username..."
-                mb={2}
+                font="16px"
                 height={1.5}
                 rounded
               />
+            </Grid>
+            <Grid xs={12} md={2}>
+              <Button
+                iconRight={<ArrowRight />}
+                type="secondary-light"
+                width="100%"
+                height={1.3}
+                rounded
+              >
+                Go!
+              </Button>
             </Grid>
           </Grid.Container>
         </Container>
